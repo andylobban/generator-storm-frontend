266a00421ddef147174b08a6f953189bc4997494
Ryan Smith <ryan.smith@stormid.com>
2014-05-22 17:09:22 +0100

    added initial grunt config

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        //pkg: grunt.file.readJSON('package.json'),
+        //concat: {}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
+    
+};
