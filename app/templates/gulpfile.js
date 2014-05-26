443d16e6e004d9aa6603ef1001da65f4bea50861
Ryan Smith <ryan.smith@stormid.com>
2014-05-26 13:52:33 +0100

    added Assemble.io

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,4 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
+var assemble = require('gulp-assemble');
 
+// Assemble 
+var assembleOptions = {
+  //data: 'data/*.json',
+  partials: 'templates/partials/*.hbs',
+  layoutdir: 'templates/layouts/'
+};
+
+gulp.task('assemble', function () {
+  gulp.src('templates/pages/*.hbs')
+    .pipe(assemble(assembleOptions))
+    .pipe(gulp.dest('_gh_pages/'));
+});
+
+gulp.task('default', ['assemble']);
